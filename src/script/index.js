f1086a29aaa6391c0731190e0562105cc87753b1
Hamilton Junior <hamiltonjrribeiro@hotmail.com>
2025-03-13 12:56:53 -0300

    Update index.js removing comments

--- src/script/index.js
+++ src/script/index.js
@@ -12,18 +12,3 @@ function removerAtivos() {
         question.classList.remove('active')
     })
 }
-// const questions = document.querySelectorAll('.question')
-
-// questions.forEach(question => {
-//     question.addEventListener('click', function () {
-//         const itsActive = question.classList.contains('active')
-//         questions.forEach(question => {
-//             question.classList.remove('active')
-//         })
-//         if (!itsActive) {
-//             question.classList.add('active')
-//         } else {
-//             question.classList.remove('active')
-//         }
-//     })
-// })
